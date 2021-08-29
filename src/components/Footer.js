f953164d3886e76358e35ef549258da59356c6e0
jehadabuawwad <jehadabuawwad@outlook.com>
2021-08-29 16:54:36 +0300

    Lab: Class 01 Solved

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+class Footer extends React.Component {
+    render(){
+        return(
+
+            <h1>Jehad Abu Awwad</h1>
+        )
+    }
+    
+
+
+
+
+}
+
+export default Footer;
