dfa5ff195eb7b5d7854f324fb172902aeb1cd0ac
jehadabuawwad <jehadabuawwad@outlook.com>
2021-08-31 10:19:44 +0300

    The class02 deploying

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Data from '../assest/data.json'
 import Card from 'react-bootstrap/Card'
 import Row from 'react-bootstrap/Card'
 import Col from 'react-bootstrap/Card'
@@ -9,42 +8,32 @@ class HornedBeasts extends React.Component {
   constructor(props) {
     super(props);
     this.love = '💓';
-    this.state = {timesClicked: 0};
-    this.increaseTimesClick = this.increaseTimesClick.bind(this);
+    this.state = { timesClicked: 0 };
   }
 
   increaseTimesClick = (event) => {
-    this.setState({timesClicked: this.state.timesClicked+1});
-    // this.setState({
-    //   timesClicked: this.state.timesClicked + 1
-    // });
-    
+    this.setState({ timesClicked: this.state.timesClicked + 1 });
   }
   render() {
     return (
       <div>
-        {
-          Data.map(item => {
-            return (
-              <Row xs={1} md={2} className="g-4">
-                {Array.from({ length: 1 }).map((_, idx) => (
-                  <Col>
-                    <Card>
-                      <Card.Img variant="top" src={item.image_url} onClick={this.increaseTimesClick} />
-                      <Card.Body>
-                        <Card.Title>{item.title}</Card.Title>
-                        <Card.Text>
+        <Row xs={1} md={2} className="g-4">
+          {Array.from({ length: 1 }).map((_, idx) => (
+            <Col>
+              <Card>
+                <Card.Img variant="top" src={this.props.image_url} onClick={this.increaseTimesClick} />
+                <Card.Body>
+                  <Card.Title>{this.props.title}</Card.Title>
+                  <Card.Text>
 
-                          {item.description} {this.state.timesClicked} {this.love.repeat(this.state.counter)}
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                ))}
-              </Row>
-            )
-          }
-          )};
+                    {this.props.description} {this.state.timesClicked} {this.love}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+            
       </div>
     )
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import HornedBeasts from './HornedBeasts'
+import Data from '../assest/data.json'
 
 class Main extends React.Component {
 
     render() {
-        
+        const Galary=Data.map(element=>{
+            return(        
+            <HornedBeasts
+            image_url={element.image_url}
+            title={element.title}
+            description={element.description}
+            />)
+        })
         return (
             <main>
-                <HornedBeasts/>
+                {
+                    Galary
+                }
+                
             </main>
         )
     }
